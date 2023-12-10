0c75e501ad701e16b440e80e8c7d38a0a06ff66d
Sergey Fedyanov <s.fedyanov@kryptonite.ru>
2023-12-10 04:37:15 +0300

    lesson 2 part 1

--- app/hello.go
+++ app/hello.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 func main() {
+	hello()
+	currentDate()
+	replace()
+}
+
+func hello() {
 	fmt.Println("Hello, Go!")
+}
+
+func currentDate() {
+	var now time.Time = time.Now()
+	var year int = now.Year()
+	var month time.Month = now.Month()
+	var day int = now.Day()
+	fmt.Println(now)
+	fmt.Println(year, month, day)
+}
+
+func replace() {
+	brocken := "G# r#cks!"
+	replacer := strings.NewReplacer("#", "o")
+	fixed := replacer.Replace(brocken)
+	fmt.Println(brocken, "\n", fixed)
 }
